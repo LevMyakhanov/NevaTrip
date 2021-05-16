9e49d1493e7750ebd8a3714516da63212882530d
Lev <mynameislev@mail.ru>
2021-05-16 11:51:01 +0700

    2

--- index.js
+++ index.js
@@ -5,20 +5,22 @@ let tripTimesLists = document.querySelectorAll('.card__trip-list');
 let hideTimes = function(timesParent) {
     for (let j = 0; j < timesParent.length; j++) {
         let tripTimes = timesParent[j].querySelectorAll('.card__trip-item');
-        if(tripTimes.length > 4) {
+        if(tripTimes.length > 3) {
             for (let i = 0; i < tripTimes.length; i++) {
-                if(i > 3) {
+                if(i > 2) {
                     tripTimes[i].classList.add('trip-time_disable');
                 }
             }
             let elementMore = document.createElement("li");
             elementMore.classList.add('card__trip-more');
             elementMore.textContent = "ещё.."
-            tripTimes.append(elementMore);
+            elementMore.addEventListener("click", function () {
+                tripTimes[i].classList.remove('trip-time_disable');
+            });
+
+            tripTimes[i].appendChild(elementMore);
         }
     }
 }
 
 hideTimes(tripTimesLists);
-
-console.log(tripTimes);
